62d441822ac102147e1e0045d8fb133f107858d4
TheRealPancakeman <rolands.silins345@gmail.com>
2020-05-20 18:39:53 +0300

    v0.1

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        bool xPlayerTurn = false;
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGrid();
+            InitilazeCells();
+        }
+
+        private void InitializeGrid()
+        {
+            Grid.BackColor = Color.LightCoral;
+            Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
+        }
+        private void InitilazeCells()
+        {
+            string labelName;
+            for (int i = 1; i < 10; i++)
+            {
+                labelName = "label" + i;
+                Grid.Controls[labelName].Text = string.Empty;
+            }
+        }
+
+        private void Player_Click(object sender, EventArgs e)
+        {
+            Label label = (Label)sender;
+            if (xPlayerTurn)
+            {
+                label.Text = "X";
+            }
+            else
+            {
+                label.Text = "O";
+            }
+            xPlayerTurn = !xPlayerTurn;
+        }
+    }
+}
