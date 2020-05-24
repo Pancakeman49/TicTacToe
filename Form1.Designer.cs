7edbb854f1f9b8c7eb7573ce41164c77dfcbcd56
TheRealPancakeman <rolands.silins345@gmail.com>
2020-05-24 04:01:20 +0300

    v1.5

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -58,13 +58,13 @@
             this.Grid.Controls.Add(this.label1, 0, 0);
             this.Grid.Dock = System.Windows.Forms.DockStyle.Fill;
             this.Grid.Location = new System.Drawing.Point(0, 0);
-            this.Grid.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Grid.Margin = new System.Windows.Forms.Padding(2);
             this.Grid.Name = "Grid";
             this.Grid.RowCount = 3;
             this.Grid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.Grid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.Grid.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.Grid.Size = new System.Drawing.Size(692, 519);
+            this.Grid.Size = new System.Drawing.Size(905, 484);
             this.Grid.TabIndex = 0;
             // 
             // label9
@@ -72,10 +72,10 @@
             this.label9.AutoSize = true;
             this.label9.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(462, 346);
+            this.label9.Location = new System.Drawing.Point(604, 322);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(228, 173);
+            this.label9.Size = new System.Drawing.Size(299, 162);
             this.label9.TabIndex = 8;
             this.label9.Text = "D";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -86,10 +86,10 @@
             this.label8.AutoSize = true;
             this.label8.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(232, 346);
+            this.label8.Location = new System.Drawing.Point(303, 322);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(226, 173);
+            this.label8.Size = new System.Drawing.Size(297, 162);
             this.label8.TabIndex = 7;
             this.label8.Text = "D";
             this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -100,10 +100,10 @@
             this.label7.AutoSize = true;
             this.label7.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(2, 346);
+            this.label7.Location = new System.Drawing.Point(2, 322);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(226, 173);
+            this.label7.Size = new System.Drawing.Size(297, 162);
             this.label7.TabIndex = 6;
             this.label7.Text = "D";
             this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -114,10 +114,10 @@
             this.label6.AutoSize = true;
             this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(462, 173);
+            this.label6.Location = new System.Drawing.Point(604, 161);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(228, 173);
+            this.label6.Size = new System.Drawing.Size(299, 161);
             this.label6.TabIndex = 5;
             this.label6.Text = "D";
             this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -128,10 +128,10 @@
             this.label5.AutoSize = true;
             this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(232, 173);
+            this.label5.Location = new System.Drawing.Point(303, 161);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(226, 173);
+            this.label5.Size = new System.Drawing.Size(297, 161);
             this.label5.TabIndex = 4;
             this.label5.Text = "D";
             this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -142,10 +142,10 @@
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(2, 173);
+            this.label4.Location = new System.Drawing.Point(2, 161);
             this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(226, 173);
+            this.label4.Size = new System.Drawing.Size(297, 161);
             this.label4.TabIndex = 3;
             this.label4.Text = "D";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -156,10 +156,10 @@
             this.label3.AutoSize = true;
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(462, 0);
+            this.label3.Location = new System.Drawing.Point(604, 0);
             this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(228, 173);
+            this.label3.Size = new System.Drawing.Size(299, 161);
             this.label3.TabIndex = 2;
             this.label3.Text = "D";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -170,10 +170,10 @@
             this.label2.AutoSize = true;
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(232, 0);
+            this.label2.Location = new System.Drawing.Point(303, 0);
             this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(226, 173);
+            this.label2.Size = new System.Drawing.Size(297, 161);
             this.label2.TabIndex = 1;
             this.label2.Text = "D";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -187,7 +187,7 @@
             this.label1.Location = new System.Drawing.Point(2, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(226, 173);
+            this.label1.Size = new System.Drawing.Size(297, 161);
             this.label1.TabIndex = 0;
             this.label1.Text = "D";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -197,9 +197,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(692, 519);
+            this.ClientSize = new System.Drawing.Size(905, 484);
             this.Controls.Add(this.Grid);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Grid.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,20 @@ namespace TicTacToe
     public partial class Form1 : Form
     {
         bool xPlayerTurn = false;
+        Button button = new Button();
+        Label labelend = new Label();
+        string winner;
         public Form1()
         {
             InitializeComponent();
             InitializeGrid();
             InitilazeCells();
+            CreateButtonNLabel();
         }
 
         private void InitializeGrid()
         {
+            Grid.Show();
             Grid.BackColor = Color.LightCoral;
             Grid.CellBorderStyle = TableLayoutPanelCellBorderStyle.Inset;
         }
@@ -34,19 +40,140 @@ namespace TicTacToe
                 Grid.Controls[labelName].Text = string.Empty;
             }
         }
+        private void CreateButtonNLabel()
+        {
+            Controls.Add(button);
+            button.Location = new Point(ClientSize.Width / 2 - button.Width, ClientSize.Height / 2 - button.Height);
+            button.Width = 100;
+            button.Height = 50;
+            button.Text = "Reset";
+            button.Anchor = (AnchorStyles.None);
+            button.Click += new EventHandler(Button_Click);
+            button.Hide();
+
+            Controls.Add(labelend);
+            labelend.Width = 200;
+            labelend.Height = 200;
+            labelend.Location = new Point(ClientSize.Width / 2 - button.Width + 10, ClientSize.Height / 2 - button.Height - 50);
+            labelend.Font = new Font("Courier New", 30);
+            labelend.Anchor = (AnchorStyles.None);
+            labelend.Hide();
+        }
 
         private void Player_Click(object sender, EventArgs e)
         {
             Label label = (Label)sender;
+            if (label.Text == string.Empty)
+            {
+                if (xPlayerTurn)
+                {
+                    label.Text = "X";
+                }
+                else
+                {
+                    label.Text = "O";
+                }
+                WinCheck();
+                DrawCheck();
+                xPlayerTurn = !xPlayerTurn;
+            }
+
+        }
+        private void WinCheck()
+        {
+            string labelName2;
+            string vLine = null;
+            string hLine1 = null;
+            string hLine2 = null;
+            string hLine3 = null;
+            string dLine1 = null;
+            string dLine2 = null;
+            for (int i = 1; i < 10; i++)
+            {
+                labelName2 = "label" + i;
+                vLine += Grid.Controls[labelName2].Text;
+                if (vLine == "XXX" || vLine == "OOO")
+                {
+                    GameOver();
+                    return;
+                }
+                if (i == 3 || i == 6)
+                {
+                    vLine = string.Empty;
+                }
+                if (i == 1 || i == 4 || i == 7)
+                {
+                    hLine1 += Grid.Controls[labelName2].Text;
+                }
+                if (i == 2 || i == 5 || i == 8)
+                {
+                    hLine2 += Grid.Controls[labelName2].Text;
+                }
+                if (i == 3 || i == 6 || i == 9)
+                {
+                    hLine3 += Grid.Controls[labelName2].Text;        // this is worse than just writing all the options label2 == labe2 && label2 == label3 ...
+                }
+                if (i == 1 || i == 5 || i == 9)
+                {
+                    dLine1 += Grid.Controls[labelName2].Text;
+                }
+                if (i == 3 || i == 5 || i == 7)
+                {
+                    dLine2 += Grid.Controls[labelName2].Text;
+                }
+                if (hLine1 == "XXX" || hLine1 == "OOO" || 
+                    hLine2 == "XXX" || hLine2 == "OOO" || 
+                    hLine3 == "XXX" || hLine3 == "OOO" || 
+                    dLine1 == "XXX" || dLine1 == "OOO" || 
+                    dLine2 == "XXX" || dLine2 == "OOO")
+                {
+                    GameOver();
+                    return;
+                }
+            }
+        }
+        private void DrawCheck()
+        {
+            if (label1.Text != string.Empty &&
+                label2.Text != string.Empty &&
+                label3.Text != string.Empty &&
+                label4.Text != string.Empty &&
+                label5.Text != string.Empty &&
+                label6.Text != string.Empty &&
+                label7.Text != string.Empty &&
+                label8.Text != string.Empty && // i hate this
+                label9.Text != string.Empty)
+            {
+                labelend.Text = "Draw :(";
+                GameReset();
+            }
+        }
+        private void GameOver()
+        {
             if (xPlayerTurn)
             {
-                label.Text = "X";
+                winner = "X";
             }
             else
             {
-                label.Text = "O";
+                winner = "O";
             }
-            xPlayerTurn = !xPlayerTurn;
+            labelend.Text = winner + " Wins";
+            GameReset();
+        }
+        private void GameReset()
+        {
+            Grid.Hide();
+            button.Show();
+            labelend.Show();
+
+        }
+        private void Button_Click(object sender, EventArgs e)
+        {
+            InitializeGrid();
+            InitilazeCells();
+            button.Hide();
+            labelend.Hide();
         }
     }
 }
